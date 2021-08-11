658277f7c7b0a9b2f01a24d05edbd23d3b496e0f
Gustavo Batista <gustavobatistafig@hotmail.com>
2021-08-11 16:38:33 -0300

    Using Swagger Documentation

--- src/main/java/com/batista/loja/LojaApplication.java
+++ src/main/java/com/batista/loja/LojaApplication.java
@@ -1,29 +1,13 @@
 package com.batista.loja;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
-	}
-	
-	@Bean
-	public DataSource dataSource() {
-
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost/lojadb?useTimezone=true&serverTimezone=UTC");
-		dataSource.setUsername("batista");
-		dataSource.setPassword("root");
-		return dataSource;
-
 	}
 
 }
